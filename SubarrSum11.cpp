1486002627ca6fea76a73b32ed28f2c2787482e4
Smita Gupta <smitagupta2858@gmail.com>
2026-02-12 12:22:10 +0530

    Add files via upload

---
+++ SubarrSum11.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void printSubarrays(int *arr,int n){
+    for(int st=0;st<n;st++){
+        for(int end=st;end<n;end++){
+            for(int i=st;i<=end;i++){
+                cout<<arr[i];
+            }
+            cout<<",";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(int);
+    printSubarrays(arr,n);
+return 0;
+}
