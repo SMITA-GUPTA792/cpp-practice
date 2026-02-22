9eebc5fd77708045131aef41d34fc67375cad7b5
Smita Gupta <smitagupta2858@gmail.com>
2026-02-22 15:19:11 +0530

    Add files via upload

---
+++ insertionSortpart2Day12.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void insertionSort(int arr[],int n){                         //INSERTION SORTING----T.C=O(n^2)
+    for(int i=1;i<n;i++){
+        int curr=arr[i];
+        int prev=i-1;
+        while(prev>=0&&arr[prev]>curr){
+            swap(arr[prev],arr[prev+1]);
+            prev--;
+        }
+        arr[prev+1]=curr;
+    }
+
+}
+
+
+int main(){
+    int arr[]={5,4,1,3,2};
+    insertionSort(arr,5);
+    print(arr,5);
+return 0;
+}
