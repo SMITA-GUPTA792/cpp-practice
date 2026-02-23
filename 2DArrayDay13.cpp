1c2d3575f6a046b910b8fbc3a9911042abd433d9
Smita Gupta <smitagupta2858@gmail.com>
2026-02-23 12:03:37 +0530

    Implement spiral matrix printing function
    Added a function to print a matrix in spiral order.

--- 2DArrayDay13.cpp
+++ 2DArrayDay13.cpp
@@ -1,7 +1,42 @@
 #include<iostream>
 using namespace std;
+
+void spiralMatrix(int mat[][4],int n,int m){
+    int srow=0,scol=0;
+    int erow=n-1,ecol=m-1;
+
+    while(srow<=erow&&scol<=ecol){
+        //TOP
+        for(int j=scol;j<=ecol;j++){
+            cout<<mat[srow][j]<<" ";
+        }
+        //RIGHT
+        for(int i=srow+1;i<=erow;i++){
+            cout<<mat[i][ecol]<<" ";
+        }
+        //BOTTOM
+        for(int j=ecol-1;j>=scol;j--){
+            if(srow==erow){
+                break;
+            }
+            cout<<mat[erow][j]<<" ";
+        }
+        //LEFT
+        for(int i=erow-1;i>=srow+1;i--){
+            if(scol==ecol){
+                break;
+            }
+            cout<<mat[i][scol]<<" ";
+        }
+        srow++;scol++;
+        erow--;ecol--;
+    }
+}
 int main(){
-    int student[3][3]={{100,100,100},{85,74,89},{63,72,65}};
+    int matrix[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+    spiralMatrix(matrix,3,4);
+    
+    /*int student[3][3]={{100,100,100},{85,74,89},{63,72,65}};
     cout<<student[1][1]<<endl;
 
     int arr[3][4];
@@ -16,6 +51,6 @@ int main(){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
-    }
+    }*/
 return 0;
 }
