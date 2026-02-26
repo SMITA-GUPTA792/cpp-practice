331477a9bf1a66079c19b5aea6db8a53cbb460d9
Smita Gupta <smitagupta2858@gmail.com>
2026-02-26 18:27:51 +0530

    Implement brute force search for matrix key lookup
    Added brute force search method to find key in matrix.

--- searchInSortedArrayDay13.cpp
+++ searchInSortedArrayDay13.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-bool search(int mat[][4],int n,int m,int key){
+bool search(int mat[][4],int n,int m,int key){                               //STAIR CASE SEARCH METHOD---T.CO(n+m)
     int r=0;
     int c=n-1;
         while(r<n && c>=0){
@@ -18,10 +18,24 @@ bool search(int mat[][4],int n,int m,int key){
         return false;
 }
 
+bool bruteforceSearch(int mat[][4],int n,int m,int key){                //BRUTE FORCE APPROACH---T.C=O(n*m)
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==key){
+                cout<<"Found at cell (" <<i<<","<<j<<")\n";
+                return true;
+            }
+        }
+    }
+    cout<<"Key not found\n";
+    return false;
+}
 
 int main(){
     int matrix[4][4]={{10,20,30,40},{15,25,35,45},
                     {27,29,37,48},{32,33,39,50}};
     search(matrix,4,4,33);
+    bruteforceSearch(matrix,4,4,129);
 return 0;
+
 }
